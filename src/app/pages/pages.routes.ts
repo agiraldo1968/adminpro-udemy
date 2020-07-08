45ffb1682b174ece01ceb36647a2901ee988a5be
agiraldo1968 <afgir@hotmail.com>
2020-07-08 13:51:57 -0500

    Sección 8

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -4,16 +4,20 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 const pagesRoutes: Routes = [
     { 
         path: '', 
         component: PagesComponent,
         children: [
-            { path: 'dashboard', component: DashboardComponent},
-            { path: 'progress', component: ProgressComponent},
-            { path: 'graficas1', component: Graficas1Component},
-            { path: 'account-settings', component: AccountSettingsComponent},
+            { path: 'dashboard', component: DashboardComponent, data: { Titulo: 'Dashboard'}},
+            { path: 'progress', component: ProgressComponent, data: { Titulo: 'Progress'}},
+            { path: 'graficas1', component: Graficas1Component, data: { Titulo: 'Gráficas'}},
+            { path: 'promesas', component: PromesasComponent, data: { Titulo: 'Promesas'}},
+            { path: 'rxjs', component: RxjsComponent, data: { Titulo: 'RxJs'}},
+            { path: 'account-settings', component: AccountSettingsComponent, data: { Titulo: 'Ajustes del tema'}},
             { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
         ]
     }
